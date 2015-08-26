06349b6ae826be97c7d13d318a299039458194cd
Chuanren Wu <Chuanren.Wu@kit.edu>
2015-08-26 18:50:35 +0200

    continued

--- src/ast.cc
+++ src/ast.cc
@@ -1,6 +1,7 @@
 #include "ast.h"
 #include <memory>
 #include <string>
+#include <cassert>
 
 Ast::Ast() : t(Ast::T::UNKNOWN)
 {
@@ -68,4 +69,213 @@ std::set<std::string> symbols(const Ast::Ptr &p)
     std::set<std::string> s;
     symbolsImpl(p, s);
     return s;
+}
+
+static Ast::Ptr aadd(
+    const Ast::Ptr &l,
+    const Ast::Ptr &r,
+    const Ast::Dict &dict,
+    std::string &msg
+);
+
+static Ast::Ptr asub(
+    const Ast::Ptr &l,
+    const Ast::Ptr &r,
+    const Ast::Dict &dict,
+    std::string &msg
+);
+
+static Ast::Ptr amul(
+    const Ast::Ptr &l,
+    const Ast::Ptr &r,
+    const Ast::Dict &dict,
+    std::string &msg
+);
+
+static Ast::Ptr adiv(
+    const Ast::Ptr &l,
+    const Ast::Ptr &r,
+    const Ast::Dict &dict,
+    std::string &msg
+);
+
+static Ast::Ptr amod(
+    const Ast::Ptr &l,
+    const Ast::Ptr &r,
+    const Ast::Dict &dict,
+    std::string &msg
+);
+
+static Ast::Ptr apow(
+    const Ast::Ptr &l,
+    const Ast::Ptr &r,
+    const Ast::Dict &dict,
+    std::string &msg
+);
+
+static Ast::Ptr land(
+    const Ast::Ptr &l,
+    const Ast::Ptr &r,
+    const Ast::Dict &dict,
+    std::string &msg
+);
+
+static Ast::Ptr lor(
+    const Ast::Ptr &l,
+    const Ast::Ptr &r,
+    const Ast::Dict &dict,
+    std::string &msg
+);
+
+static Ast::Ptr lnot(
+    const Ast::Ptr &l,
+    const Ast::Ptr &r,
+    const Ast::Dict &dict,
+    std::string &msg
+);
+
+static Ast::Ptr ceq(
+    const Ast::Ptr &l,
+    const Ast::Ptr &r,
+    const Ast::Dict &dict,
+    std::string &msg
+);
+
+static Ast::Ptr cne(
+    const Ast::Ptr &l,
+    const Ast::Ptr &r,
+    const Ast::Dict &dict,
+    std::string &msg
+);
+
+static Ast::Ptr cgt(
+    const Ast::Ptr &l,
+    const Ast::Ptr &r,
+    const Ast::Dict &dict,
+    std::string &msg
+);
+
+static Ast::Ptr cge(
+    const Ast::Ptr &l,
+    const Ast::Ptr &r,
+    const Ast::Dict &dict,
+    std::string &msg
+);
+
+static Ast::Ptr clt(
+    const Ast::Ptr &l,
+    const Ast::Ptr &r,
+    const Ast::Dict &dict,
+    std::string &msg
+);
+
+static Ast::Ptr cle(
+    const Ast::Ptr &l,
+    const Ast::Ptr &r,
+    const Ast::Dict &dict,
+    std::string &msg
+);
+
+static Ast::Ptr
+opEval(const Ast::Ptr &root, const Ast::Dict &dict,  std::string &msg)
+{
+    assert(root->t == Ast::T::OPERATOR);
+    auto l = eval(root->left, dict, msg);
+    if (!l) {
+        return nullptr;
+    }
+    auto r = eval(root->right, dict, msg);
+    if (!r) {
+        return nullptr;
+    }
+    switch (root->op) {
+        case Ast::O::PLUS:
+            return aadd(l,r,dict,msg);
+        case Ast::O::MINUS:
+            return asub(l,r,dict,msg);
+        case Ast::O::MULTIPLY:
+            return amul(l,r,dict,msg);
+        case Ast::O::DIVISION:
+            return adiv(l,r,dict,msg);
+        case Ast::O::MODULUS:
+            return amod(l,r,dict,msg);
+        case Ast::O::POWER:
+            return apow(l,r,dict,msg);
+        case Ast::O::LOGICAL_AND:
+            return land(l,r,dict,msg);
+        case Ast::O::LOGICAL_OR:
+            return lor(l,r,dict,msg);
+        case Ast::O::LOGICAL_NOT:
+            return lnot(l,r,dict,msg);
+        case Ast::O::CMP_EQ:
+            return ceq(l,r,dict,msg);
+        case Ast::O::CMP_NE:
+            return cne(l,r,dict,msg);
+        case Ast::O::CMP_GT:
+            return cgt(l,r,dict,msg);
+        case Ast::O::CMP_GE:
+            return cge(l,r,dict,msg);
+        case Ast::O::CMP_LT:
+            return clt(l,r,dict,msg);
+        case Ast::O::CMP_LE:
+            return cle(l,r,dict,msg);
+        default:
+            return nullptr;
+    }
+}
+
+Ast::Ptr eval(const Ast::Ptr &root, const Ast::Dict &dict, std::string &msg)
+{
+    if (!root) {
+        return nullptr;
+    }
+    switch (root->t) {
+        case Ast::T::BOOLEAN:
+        case Ast::T::NUMBER:
+        case Ast::T::STRING:
+            return root->clone();
+        case Ast::T::SYMBOL: {
+            const auto i = dict.find(root->str);
+            if (i == dict.cend()) {
+                msg = "unsolvable symbol ";
+                msg += root->str;
+            }
+            return i->second.clone();
+        }
+        case Ast::T::OPERATOR:
+            return opEval(root, dict, msg);
+        default:
+            return nullptr;
+    }
+}
+
+Ast::Ptr Ast::clone() const
+{
+    return Ast::Ptr(new Ast(*this));
+}
+
+Ast::Ast(const Ast &root)
+{
+    t = root.t;
+    switch (t) {
+        case T::STRING:
+        case T::SYMBOL:
+            str = root.str;
+            break;
+        case T::NUMBER:
+            num = root.num;
+            break;
+        case T::OPERATOR:
+            op = root.op;
+            break;
+        case T::BOOLEAN:
+            b = root.b;
+            break;
+    }
+    if (root.left) {
+        left.reset(new Ast(*(root.left)));
+    }
+    if (root.right) {
+        right.reset(new Ast(*(root.right)));
+    }
 }
--- src/ast.h
+++ src/ast.h
@@ -4,9 +4,11 @@
 #include <memory>
 #include <string>
 #include <set>
+#include <map>
 
 struct Ast
 {
+    typedef std::map<std::string, Ast> Dict;
     typedef std::unique_ptr<Ast> Ptr;
     enum class T {UNKNOWN, SYMBOL, NUMBER, STRING, OPERATOR, BOOLEAN};
     enum class O {
@@ -15,10 +17,12 @@ struct Ast
         CMP_EQ, CMP_NE, CMP_GT, CMP_GE, CMP_LT, CMP_LE,
     };
     Ast();
+    Ast(const Ast &);
     explicit Ast(bool b);
     explicit Ast(double v);
     explicit Ast(const std::string &s);
     Ast(O o);
+    std::unique_ptr<Ast> clone() const;
     static std::unique_ptr<Ast> make(double v);
     static std::unique_ptr<Ast> make(O o);
     static std::unique_ptr<Ast> make(bool b);
@@ -36,5 +40,6 @@ struct Ast
 };
 
 std::set<std::string> symbols(const Ast::Ptr &p);
+Ast::Ptr eval(const Ast::Ptr &, const Ast::Dict &, std::string &msg);
 
 #endif
